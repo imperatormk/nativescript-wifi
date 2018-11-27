ccd495ddd56715cfd0fec1f2c0d45c118d7b0e10
Darko <darko.simonovski@bestisoft.com>
2018-11-27 07:05:14 +0100

    Implemented function wrappers

--- index.js
+++ index.js
@@ -9,5 +9,29 @@ module.exports = {
     getLocalIpAddress: function() {
         var toaster = new tv.channelhopper.ipsync.toaster.Toaster();
         return toaster.getLocalIpAddress(context);
+    },
+    getLocalIpv4A: function() {
+        var toaster = new tv.channelhopper.ipsync.toaster.Toaster();
+        return toaster.getLocalIpv4A();
+    },
+    getLocalIpV6: function() {
+        var toaster = new tv.channelhopper.ipsync.toaster.Toaster();
+        return toaster.getLocalIpV6();
+    },
+    getDeviceSubnetMaskString: function() {
+        var toaster = new tv.channelhopper.ipsync.toaster.Toaster();
+        return toaster.getDeviceSubnetMaskString(context);
+    },
+    getDeviceGatewayString: function() {
+        var toaster = new tv.channelhopper.ipsync.toaster.Toaster();
+        return toaster.getDeviceGatewayString(context);
+    },
+    getDnsAddresses: function() {
+        var toaster = new tv.channelhopper.ipsync.toaster.Toaster();
+        return toaster.getDnsAddresses(context);
+    },
+    getConnectionData: function() {
+        var toaster = new tv.channelhopper.ipsync.toaster.Toaster();
+        return toaster.getConnectionData(context);
     }
 };
